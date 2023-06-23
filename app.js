7f20926a29259adde4b258aa2fa0e45ceb7af8b3
ygor carmo <ygor.carmo@gmail.com>
2023-06-23 00:36:13 +0000

    fix typos

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 http.createServer(function (req, res){
 res.write("On the way to being a full snack engineer!");
-res.end();)
+res.end();
 
 }
-).liste(3000);
+).listen(3000);
 
 console.log("server started on port 3000");
